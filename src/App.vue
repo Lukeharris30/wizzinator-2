bd907c92f5170c50f9339ecdcf08e08ebb1faaf9
Luke Harris <8006624+Lukeharris30@users.noreply.github>
2025-08-11 16:06:14 -0600

    small style

--- src/App.vue
+++ src/App.vue
@@ -16,22 +16,51 @@
   <div>
 
 	<section>
-		<TransitionGroup name="slide"/>
-		<div v-if="currentStep === 1">Step one</div>
-		<div v-if="currentStep === 2">Step two</div>
+		<TransitionGroup name="slide">
+		<div v-if="currentStep === 1" class="step-content">Step one</div>
+		<div v-if="currentStep === 2" class="step-content">Step two</div>
+		<div v-if="currentStep === 3" class="step-content">Step three</div>
 		</TransitionGroup>
-		<transition name="fade">
-		<button v-if="currentStep > 1" @click="previousStep">Previous</button>
-		</transition>
-		<button @click="nextStep">Next</button>
+		<div class="button-group">
+		   <transition name="fade">
+		   <button v-show="currentStep > 1" @click="previousStep">&#x2190;</button>
+		   </transition>
+		   <button class="next" @click="nextStep">&#x2192;</button>
+		</div>
 	</section>
 
   </div>
 </template>
 
 <style scoped>
+section {
+	margin: auto;
+	text-align: center;
+	width: 300px;
+}
+.step-content{
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-height: 200px;
+}
+.button-group {
+	position: relative;
+	button {
+		position: absolute;
+		left: 0;
+		width: 40px;
+	}
+	button.next {
+		position: absolute;
+		right: 0;
+	}
+}
 .fade-enter-active, .fade-leave-active{
-	transition: opacity 2s ease;
+	transition: opacity 1s ease;
+}
+.fade-leave-active{
+	position: absolute;
 }
 .fade-enter-from{
 	opacity: 0;
@@ -40,10 +69,11 @@
 	opacity: 0;
 }
 .slide-enter-active, .slide-leave-active{
-	transition: all 2s ease;
+	transition: all 1s ease;
 }
 .slide-leave-active {
 	position: absolute;
+	display: none;
 }
 .slide-enter-from {
 	transform: translateX(40px);
