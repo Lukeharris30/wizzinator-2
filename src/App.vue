d2785c7f615a6db1298e653aab583a0699f8880d
Luke Harris <8006624+Lukeharris30@users.noreply.github.com>
2025-08-12 12:49:34 -0600

    feat: prototype

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,63 @@
 <script setup lang="ts">
-	import { ref } from "vue"
+	import { ref, onMounted} from "vue"
 	const currentStep = ref(1);
+	const goingUp = ref(true)
 	function nextStep() {
-		currentStep.value ++;	
+		if(currentStep.value < steps.value){
+			goingUp.value = true 
+			currentStep.value ++;	
+		}
 	}
 	function previousStep() {
 		if(currentStep.value > 1){
+			goingUp.value = false 
 			currentStep.value --
 		}
 	}
+	const steps = ref(0)
+	onMounted(() => {
+		steps.value = document.querySelectorAll('.step-content').length
+	})
+	// form data
+	const udh = ref('')
 
 </script>
 
 <template>
   <div>
-
 	<section>
-		<TransitionGroup name="slide">
-		<div v-if="currentStep === 1" class="step-content">Step one</div>
-		<div v-if="currentStep === 2" class="step-content">Step two</div>
-		<div v-if="currentStep === 3" class="step-content">Step three</div>
+		<h1>Mountain</h1>
+		<div class="steps-container">
+			<div v-for="(number, index) of steps" class="steps" :class="{selected: index === (currentStep -1)}" :key="index">{{currentStep > number ? '&#10003;' : index + 1}}</div>
+		</div>
+		<TransitionGroup :name="goingUp ? 'slide' : 'slideback'">
+			<div v-show="currentStep === 1" key="1" class="step-content">
+				<label>UDH?<input type="checkbox" v-model="udh"/></label>
+			</div>
+			<div v-show="currentStep === 2" key="2" class="step-content">
+				Step two
+			</div>
+			<div v-show="currentStep === 3" key="3" class="step-content">
+				Step three
+			</div>
 		</TransitionGroup>
 		<div class="button-group">
-		   <transition name="fade">
-		   <button v-show="currentStep > 1" @click="previousStep">&#x2190;</button>
-		   </transition>
-		   <button class="next" @click="nextStep">&#x2192;</button>
+		   	<TransitionGroup name="fade">
+				<button key="prev" class="prev" v-show="currentStep > 1" @click="previousStep">&#x2190;</button>
+				<button key="next" class="next" v-show="currentStep < steps" @click="nextStep">&#x2192;</button>
+			</TransitionGroup>
 		</div>
 	</section>
-
+	<pre>udh{{udh}}</pre>
   </div>
 </template>
 
 <style scoped>
+
 section {
 	margin: auto;
 	text-align: center;
-	width: 300px;
+	width: 40vw;
 }
 .step-content{
 	display: flex;
@@ -48,16 +69,20 @@ section {
 	position: relative;
 	button {
 		position: absolute;
-		left: 0;
 		width: 40px;
+		background: transparent;
+		padding: 4px 10px;
 	}
 	button.next {
 		position: absolute;
 		right: 0;
 	}
+	button.prev {
+		left: 0;
+	}
 }
 .fade-enter-active, .fade-leave-active{
-	transition: opacity 1s ease;
+	transition: opacity .4s ease;
 }
 .fade-leave-active{
 	position: absolute;
@@ -83,4 +108,45 @@ section {
 	transform: translateX(40px);
 	opacity: 0;
 }
+.slideback-enter-active, .slideback-leave-active{
+	transition: all 1s ease;
+}
+.slideback-leave-active {
+	position: absolute;
+	display: none;
+}
+.slideback-enter-from {
+	transform: translateX(-40px);
+	opacity: 0;
+}
+.slideback-leave-to {
+	transform: translateX(-40px);
+	opacity: 0;
+}
+.steps-container {
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+}
+.steps {
+      width: 30px;
+      height: 30px;
+      border: 1px solid black;
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+}
+.selected {
+	    background: #eee;
+}
+.steps:has(~ .selected){
+	background: red;
+	&::after{
+		content:'';
+	
+	}
+}
+
 </style>
